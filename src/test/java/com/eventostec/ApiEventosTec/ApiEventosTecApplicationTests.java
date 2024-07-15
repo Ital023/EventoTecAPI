c3efb636b9f92ebef7f504b650a3bc4a962f0d3d
Ital023 <contadoitalo911@gmail.com>
2024-07-14 23:15:52 -0300

    feat: initial files

---
+++ src/test/java/com/eventostec/ApiEventosTec/ApiEventosTecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventostec.ApiEventosTec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEventosTecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
